5685b612f82eaa13f3fc7f1bfd29a5d9304c7cdd
Jeswin A.H <jeswinah@example.com>
2025-05-15 07:40:33 +0530

    new_change

--- Server.js
+++ Server.js
@@ -1,42 +1,37 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const db = require("./Database/Database.js");
-const book = require("./Database/Model/Book.js");
-const Book = require("./Database/Model/Book.js");
-const port = process.env.port || 2000;
+const db = require("./Database/Database");
+const Book = require("./Models/Book");
 
-//Database connection
-db();
+const app = express();
 
-//middleware
+db(); // connect to MongoDB
+
+app.use(cors());
 app.use(express.json());
 
-//creating a book
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.post("/books", async (req, res) => {
+  const { title, author } = req.body;
   try {
-    const { title, author } = req.body;
-    const Newbook = new Book({ title, author });
-    const Savedbook = await Newbook.save();
-    console.log(Savedbook);
-    res.status(201).json(Savedbook);
-  } catch (error) {
-    console.log(error.message);
-    req.status(500).json(error.message);
+    const newBook = new Book({ title, author });
+    const saved = await newBook.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
 
-//Getting a book
 app.get("/books", async (req, res) => {
   try {
-    const Allbooks = await Book.find();
-    console.log(Allbooks);
-    res.status(201).json(Allbooks);
-  } catch (error) {
-    console.log(error.message);
-    req.status(500).json(error.message);
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 });
-
 
 module.exports = app;
